c412a75df13e6297949416e4afe93b22aeb01e14
Stephen Conklin <edg@thinkedg.com>
2017-02-08 19:57:52 +0000

    added a container name to make portable

--- test/ckhttpd.bash
+++ test/ckhttpd.bash
@@ -2,7 +2,7 @@
 
 echo '>>>>verifying that httpd is running in the container<<<<<'
 
-HTTPD_RUN=`docker exec -it minipro_web_server_1 ps ax | grep httpd | grep -v grep`
+HTTPD_RUN=`docker exec -it stelligent_mini_project ps ax | grep httpd | grep -v grep`
 
 echo "$HTTPD_RUN" |  grep httpd
 
